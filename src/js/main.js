6edce8614cc7ca77058d8def9a59c5863241a6e3
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-04-02 15:44:01 +0300

    добавить открытие и закрытие модального окна для вызова замерщика

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import "./slider";
+import modals from "./modules/modals";
+
+window.addEventListener("DOMContentLoaded", () => {
+  modals();
+});
+
+// 34.45
+// https://www.youtube.com/watch?v=rubOd5v9kxc
